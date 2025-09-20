5f64922d2d75754d5cc092edc0e285d978b164c4
Jing Xuan Goh <gohjx8808@gmail.com>
2025-09-21 01:20:51 +0800

    added translation for detected object name

--- cortex_api/routers/objectDetection.py
+++ cortex_api/routers/objectDetection.py
@@ -2,6 +2,7 @@ import json
 
 import cv2
 import numpy as np
+from deep_translator import GoogleTranslator
 from fastapi import APIRouter, File, UploadFile
 from pydantic import BaseModel
 from ultralytics import YOLO
@@ -18,6 +19,8 @@ model = YOLO("yolo11n.pt")
 
 @router.post("/detect")
 async def detect_objects(file: UploadFile = File(...)):
+    translator = GoogleTranslator(source="en", target="zh-TW")
+
     # Read file contents
     contents = await file.read()
     nparr = np.frombuffer(contents, np.uint8)
@@ -25,5 +28,11 @@ async def detect_objects(file: UploadFile = File(...)):
     # Decode image
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
+    # Perform object detection
     results = model(img)
-    return json.loads(results[0].to_json())
+    parsedResults = json.loads(results[0].to_json())
+
+    # Translate object names
+    for result in parsedResults:
+        result["name"] = translator.translate(result["name"])
+    return parsedResults
